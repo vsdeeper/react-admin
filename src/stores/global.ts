52c5dc294ff1a6f2542e0fbeffc53386085dedc8
vsdeeper <vsdeeper@qq.com>
2025-01-23 12:46:26 +0800

    feat: update

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -10,7 +10,6 @@ export const globalSlice = createSlice({
   initialState: {
     userInfo: {},
     menu: [],
-    routes: [],
   } as GlobalState,
   reducers: {
     storeUserInfo: (state, { payload }) => {
